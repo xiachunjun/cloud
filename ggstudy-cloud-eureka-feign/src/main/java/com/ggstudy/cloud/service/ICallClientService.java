300855761f80cfec7eeee84b5050fbc4b466f4a2
xiachunjun <xiachunjun@wanda.cn>
2017-10-17 09:10:41 +0800

    no message

--- ggstudy-cloud-eureka-feign/src/main/java/com/ggstudy/cloud/service/ICallClientService.java
+++ ggstudy-cloud-eureka-feign/src/main/java/com/ggstudy/cloud/service/ICallClientService.java
@@ -4,8 +4,11 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "eureka-client")
+import com.ggstudy.cloud.fallback.CallClientServiceFallBack;
+
+@FeignClient(value = "eureka-client", fallback = CallClientServiceFallBack.class)
 public interface ICallClientService {
+
 	@RequestMapping(value = "/port", method = RequestMethod.GET)
 	String getPortFromClientOne();
 }
