2ab5c385a6d077f2135d3980ff23a1f690078618
xiachunjun <xiachunjun@wanda.cn>
2017-10-30 17:57:39 +0800

    no message

--- ggstudy-cloud-eureka-client/src/main/java/com/ggstudy/cloud/Application.java
+++ ggstudy-cloud-eureka-client/src/main/java/com/ggstudy/cloud/Application.java
@@ -2,9 +2,8 @@ package com.ggstudy.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
